4352e8a92bfb296ecb7f346c6677bac426408b5d
MrCronkite <vladshima99@gmail.com>
2020-01-09 23:14:26 +0300

    create gulp guides

---
+++ gulp-demo/gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autopref = require("gulp-autoprefixer");
+const concat = require("gulp-concat");
+const imagemin = require("gulp-imagemin");
+const uglifyjs = require("gulp-uglifyjs");
+
+gulp.task("sass", function() {
+  gulp
+    .src("./src/sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./dist/css"));
+});
+
+gulp.task("sass:watch", function() {
+  gulp.watch("./src/sass/**/*.scss", gulp.series("sass"));
+});
